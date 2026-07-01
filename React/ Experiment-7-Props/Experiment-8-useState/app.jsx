cc347abf4a5f60571fc89540fed85f6c9e320f74
Umang Dhariwal <UmangDhariwal07@gmail.com>
2026-07-01 09:27:51 +0530

    Implement React counter using useState

---
+++ React/ Experiment-7-Props/Experiment-8-useState/app.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+
+    const [count,setCount]=useState(0);
+
+    return(
+        <div className="counter">
+
+            <h1>{count}</h1>
+
+            <button onClick={()=>setCount(count+1)}>
+                Increment
+            </button>
+
+            <button onClick={()=>setCount(count-1)}>
+                Decrement
+            </button>
+
+        </div>
+    );
+}
+
+export default App;
