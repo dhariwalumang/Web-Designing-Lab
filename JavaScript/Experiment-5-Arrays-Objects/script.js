b5b22ffaabeb491e99a635d59005054513e0cc46
Umang Dhariwal <UmangDhariwal07@gmail.com>
2026-07-01 09:09:47 +0530

    Add employee dataset

---
+++ JavaScript/Experiment-5-Arrays-Objects/script.js
@@ -0,0 +1,25 @@
+const employees = [
+
+    {
+        id:1,
+        name:"Rahul",
+        salary:45000
+    },
+
+    {
+        id:2,
+        name:"Anita",
+        salary:52000
+    },
+
+    {
+        id:3,
+        name:"Karan",
+        salary:61000
+    }
+
+];
+
+function showEmployees(){
+
+}
