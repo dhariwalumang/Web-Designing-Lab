f2a2b9549ea3192021460987f89129cb258b415a
Umang Dhariwal <UmangDhariwal07@gmail.com>
2026-07-01 09:20:55 +0530

    Render student information using props

---
+++ React/ Experiment-7-Props/app.jsx
@@ -0,0 +1,14 @@
+import Student from "./Student";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Student name="Rahul" course="B.Tech" marks="90" />
+      <Student name="Anita" course="BCA" marks="87" />
+      <Student name="Karan" course="MCA" marks="95" />
+    </>
+  );
+}
+
+export default App;
