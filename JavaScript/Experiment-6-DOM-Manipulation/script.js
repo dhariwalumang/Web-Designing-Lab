04e3b309819e5ae590e58b1890bb337fa9bb57fa
Umang Dhariwal <UmangDhariwal07@gmail.com>
2026-07-01 09:16:33 +0530

    Implement DOM manipulation functionality

---
+++ JavaScript/Experiment-6-DOM-Manipulation/script.js
@@ -0,0 +1,11 @@
+function changeText(){
+
+    const text = document.getElementById("text");
+
+    text.innerHTML = "DOM Updated Successfully!";
+
+    text.style.color = "blue";
+    text.style.fontSize = "24px";
+    text.style.fontWeight = "bold";
+
+}
